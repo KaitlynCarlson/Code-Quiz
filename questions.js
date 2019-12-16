93bb0301001d9364c6274f77806deafcfae229b2
Kaitlyn <kaitlyn.carlson7@gmail.com>
2019-12-15 20:07:22 -0800

    Creating my inital documents for Code Quiz

---
+++ questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title : "Varaibles are the _________ of programming.",
+        choices : ["var","nouns","functions", "camel back"],
+        answer : "nouns"
+    },
+    {
+        title : "What is the name of the value, in the context of JavaScript objects?",
+        choices : ["null", "var","array","property"],
+        answer : "property"
+    },
+    {
+        title : "The ______ operator invokes the function.",
+        choices : ["i++","return","()","formula"],
+        answer : "()"
+    },
+    {
+        title : "Which of the following is not a Boolean Operator?",
+        choices : ["||", "{}", "&&","!"],
+        answer : "{}"
+    }
+];
---
+++ script.js
@@ -0,0 +1,17 @@
+function quiz (){
+// Call functions up here
+// Clicking the start quiz button displays the first ((key up event on start quiz)) question and starts countdown timer
+
+// Function to run through questions
+// Final question  -- after response is logged display prompt to enter initials and display results
+
+
+// Counter function -- if answer is incorrect deduct 10 seconds from time (40 seconds total with 4 questions)
+// Time remaining = user score -- store score on local storage
+
+
+
+
+
+
+}
