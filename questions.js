c93f03d411a243a2558841f7493ea97967b09f2d
Kaitlyn <kaitlyn.carlson7@gmail.com>
2019-12-20 15:35:10 -0800

    Button bug fixed. Framework for displaying questions re worked.

--- questions.js
+++ questions.js
@@ -1,22 +1,23 @@
 var questions = [
-    {
-        title : "Variables are the _________ of programming.",
-        choices : ["var","nouns","functions", "camel back"],
-        answer : "nouns"
-    },
-    {
-        title : "What is the 'name' of a value, in the context of JavaScript objects?",
-        choices : ["null", "var","array","property"],
-        answer : "property"
-    },
-    {
-        title : "The ______ operator invokes the function.",
-        choices : ["i++","return","()","formula"],
-        answer : "()"
-    },
-    {
-        title : "Which of the following is not a Boolean Operator?",
-        choices : ["||", "{}", "&&","!"],
-        answer : "{}"
-    }
+  {
+    title: "Variables are the _________ of programming.",
+    choices: ["var", "nouns", "functions", "camel back"],
+    answer: "nouns"
+  },
+  {
+    title:
+      "What is the 'name' of a value, in the context of JavaScript objects?",
+    choices: ["null", "var", "array", "property"],
+    answer: "property"
+  },
+  {
+    title: "The ______ operator invokes the function.",
+    choices: ["i++", "return", "()", "formula"],
+    answer: "()"
+  },
+  {
+    title: "Which of the following is not a Boolean Operator?",
+    choices: ["||", "{}", "&&", "!"],
+    answer: "{}"
+  }
 ];
--- script.js
+++ script.js
@@ -15,99 +15,87 @@ function setTime() {
 }
 document.getElementById("start-quiz").addEventListener("click", setTime);
 
-// Quiz questions variables
+// Quiz question variables
 
 var accessQuestions = questions;
-console.log(accessQuestions);
+var questionOne = questions[0].title;
+var questionTwo = questions[1].title;
+var questionThree = questions[2].title;
+var questionFour = questions[3].title;
+var choicesOne = questions[0].choices;
+var choicesTwo = questions[1].choices;
+var choicesThree = questions[2].choices;
+var choicesFour = questions[3].choices;
+var answerOne = questions[0].answer;
+var answerTwo = questions[1].answer;
+var answerThree = questions[2].answer;
+var answerFour = questions[3].answer;
 
-$("#start-quiz").on("click", function() {
-  // Hide Code Quiz Home Jumbotron
+$("#questions-jumbotron").hide();
+
+$("#start-quiz").on("click", function firstSet() {
   $("#home").hide();
+  $("#questions-jumbotron").show();
 
-  // Create new <div> with class Jumbotron to display questions when start-quiz button is clicked and appending to container div
+  $("#questionDisplay").text(questionOne);
+  $("#index-zero")
+    .text(choicesOne[0])
+    .addClass("choices-one");
+  $("#index-one")
+    .text(choicesOne[1])
+    .addClass("choices-one");
+  $("#index-two")
+    .text(choicesOne[2])
+    .addClass("choices-one");
+  $("#index-three")
+    .text(choicesOne[3])
+    .addClass("choices-one");
 
-  var questionDisplay = $("<div>");
-  questionDisplay.addClass("jumbotron questions");
-  $(".container").append(questionDisplay);
-  $(questionDisplay).show();
+  $(".choices-one").on("click", function() {
+    $("#questionDisplay").text(questionTwo);
+    $("#index-zero")
+      .text(choicesTwo[0])
+      .addClass("choices-two");
+    $("#index-one")
+      .text(choicesTwo[1])
+      .addClass("choices-two");
+    $("#index-two")
+      .text(choicesTwo[2])
+      .addClass("choices-two");
+    $("#index-three")
+      .text(choicesTwo[3])
+      .addClass("choices-two");
 
-  // Display first question "title"
-  var questionOne = $("<h1>");
-  questionOne.addClass("display-4");
-  questionOne.text(accessQuestions[0].title);
-  $(questionDisplay).append(questionOne);
+    $(".choices-two").on("click", function() {
+      $("#questionDisplay").text(questionThree);
+      $("#index-zero")
+        .text(choicesThree[0])
+        .addClass("choices-three");
+      $("#index-one")
+        .text(choicesThree[1])
+        .addClass("choices-three");
+      $("#index-two")
+        .text(choicesThree[2])
+        .addClass("choices-three");
+      $("#index-three")
+        .text(choicesThree[3])
+        .addClass("choices-three");
 
-  // Display first question "choices"
-  for (var i = 0; i < accessQuestions[0].choices.length; ++i) {
-    var choicesOne = $("<button></button>  <br><br>");
-    choicesOne.addClass("btn btn-outline-dark choices-one");
-    choicesOne.text(accessQuestions[0].choices[i]);
-    $(questionDisplay).append(choicesOne);
-
-    $(choicesOne).on("click", function() {
-      // Hide question one content
-      $(questionOne).hide();
-      $(".choices-one").hide();
-
-      // Create question two content
-
-      //Display question two
-      var questionTwo = $("<h1>");
-      questionTwo.addClass("display-4");
-      questionTwo.text(accessQuestions[1].title);
-      $(questionDisplay).append(questionTwo);
-
-      // Display question two "choices"
-      for (var i = 0; i < accessQuestions[1].choices.length; ++i) {
-        var choicesTwo = $("<button></button>  <br><br>");
-        choicesTwo.addClass("btn btn-outline-dark choices-two");
-        choicesTwo.text(accessQuestions[1].choices[i]);
-        $(questionDisplay).append(choicesTwo);
-      }
-
-      $(choicesTwo).on("click", function() {
-        // Hide question one content
-        $(questionTwo).hide();
-        $(".choices-two").hide();
-
-        // Create question three content
-
-        //Display question three
-        var questionThree = $("<h1>");
-        questionThree.addClass("display-4");
-        questionThree.text(accessQuestions[2].title);
-        $(questionDisplay).append(questionThree);
-
-        // Display question two "choices"
-        for (var i = 0; i < accessQuestions[2].choices.length; ++i) {
-          var choicesThree = $("<button></button>  <br><br>");
-          choicesThree.addClass("btn btn-outline-dark choices-three");
-          choicesThree.text(accessQuestions[2].choices[i]);
-          $(questionDisplay).append(choicesThree);
-        }
-
-        $(choicesThree).on("click", function() {
-          // Hide question one content
-          $(questionThree).hide();
-          $(".choices-three").hide();
-
-          // Create question four content
-
-          //Display question four
-          var questionFour = $("<h1>");
-          questionFour.addClass("display-4");
-          questionFour.text(accessQuestions[3].title);
-          $(questionDisplay).append(questionFour);
-
-          // Display question two "choices"
-          for (var i = 0; i < accessQuestions[3].choices.length; ++i) {
-            var choicesFour = $("<button></button>  <br><br>");
-            choicesFour.addClass("btn btn-outline-dark choices-three");
-            choicesFour.text(accessQuestions[3].choices[i]);
-            $(questionDisplay).append(choicesFour);
-          }
-        });
+      $(".choices-three").on("click", function() {
+        $("#questionDisplay").text(questionFour);
+        $("#index-zero")
+          .text(choiceFour[0])
+          .addClass("choices-four");
+        $("#index-one")
+          .text(choicesFour[1])
+          .addClass("choices-four");
+        $("#index-two")
+          .text(choiceFour[2])
+          .addClass("choices-four");
+        $("#index-three")
+          .text(choiceFour[3])
+          .addClass("choices-four");
       });
     });
-  }
+  });
 });
