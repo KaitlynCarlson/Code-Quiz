fe72b62be765765c88dfcfb4103f79963569abc9
Kaitlyn <kaitlyn.carlson7@gmail.com>
2019-12-21 20:53:19 -0800

    This is a terrible commit because I made a ton of changes and just plowed on through without committing.

--- script.js
+++ script.js
@@ -1,149 +1,108 @@
-var accessQuestions = questions;
-var questionOne = questions[0].title;
-var questionTwo = questions[1].title;
-var questionThree = questions[2].title;
-var questionFour = questions[3].title;
-var choicesOne = questions[0].choices;
-var choicesTwo = questions[1].choices;
-var choicesThree = questions[2].choices;
-var choicesFour = questions[3].choices;
-var answerOne = questions[0].answer;
-var answerTwo = questions[1].answer;
-var answerThree = questions[2].answer;
-var answerFour = questions[3].answer;
-var secondsLeft;
-var countDown;
+$(document).ready(function() {
+  $("#questions-jumbotron").hide();
+  $("#quizCompleted").hide();
+  $("#history").hide();
 
-$("#questions-jumbotron").hide();
-$("#quizCompleted").hide();
-$("#history").hide();
+  // Global Variables
+  var questionsIndex = 0;
+  var timerEl = $("#time-remaining");
+  var secondsLeft = questions.length * 10;
+  var countDown;
 
-$("#start-quiz").on("click", function firstSet() {
-  $("#home").hide();
+  $("#start-quiz").on("click", function firstSet() {
+    $("#home").hide();
+    $("#questions-jumbotron").show();
 
-  $("#questions-jumbotron").show();
-
-  // Set time function
-
-  secondsLeft = 40;
-  setInterval(function() {
-    --secondsLeft;
-
-    if (secondsLeft >= 0) {
-      $("#time-remaining").text("Time remaining " + secondsLeft + " remaining");
-    }
-    if (secondsLeft < 0) {
-      secondsLeft = 0;
-      $("#time-remaining").text("Time remaining " + secondsLeft + " remaining");
-    }
-
-    if ($("#questionAccuracy").text() === "Incorrect!") {
-      secondsLeft = secondsLeft - 10;
-    }
-  }, 1000);
-
-  // function penaltyDeduct() {
-  //   secondsLeft -= 10;
-  // }
-
-  $("#questionDisplay").text(questionOne);
-  $("#index-zero")
-    .text(choicesOne[0])
-    .addClass("choices-one");
-  $("#index-one")
-    .text(choicesOne[1])
-    .addClass("choices-one");
-  $("#index-two")
-    .text(choicesOne[2])
-    .addClass("choices-one");
-  $("#index-three")
-    .text(choicesOne[3])
-    .addClass("choices-one");
-
-  $(".choices-one").on("click", function() {
-    if ($(this).text() !== answerOne) {
-      $("#questionAccuracy").text("Incorrect!");
-    } else if ($(this).text() === answerOne) {
-      $("#questionAccuracy").text("Correct!");
-    }
-
-    $("#questionDisplay").text(questionTwo);
-    $("#index-zero")
-      .text(choicesTwo[0])
-      .addClass("choices-two");
-    $("#index-one")
-      .text(choicesTwo[1])
-      .addClass("choices-two");
-    $("#index-two")
-      .text(choicesTwo[2])
-      .addClass("choices-two");
-    $("#index-three")
-      .text(choicesTwo[3])
-      .addClass("choices-two");
-
-    $(".choices-two").on("click", function() {
-      if ($(this).text() !== answerTwo) {
-        $("#questionAccuracy").text("Incorrect!");
-      } else if ($(this).text() === answerTwo) {
-        $("#questionAccuracy").text("Correct!");
-      }
-
-      $("#questionDisplay").text(questionThree);
-      $("#index-zero")
-        .text(choicesThree[0])
-        .addClass("choices-three");
-      $("#index-one")
-        .text(choicesThree[1])
-        .addClass("choices-three");
-      $("#index-two")
-        .text(choicesThree[2])
-        .addClass("choices-three");
-      $("#index-three")
-        .text(choicesThree[3])
-        .addClass("choices-three");
-
-      $(".choices-three").on("click", function() {
-        if ($(this).text() !== answerThree) {
-          $("#questionAccuracy").text("Incorrect!");
-        } else if ($(this).text() === answerThree) {
-          $("#questionAccuracy").text("Correct!");
-        }
-
-        $("#questionDisplay").text(questionFour);
-        $("#index-zero")
-          .text(choicesFour[0])
-          .addClass("choices-four");
-        $("#index-one")
-          .text(choicesFour[1])
-          .addClass("choices-four");
-        $("#index-two")
-          .text(choicesFour[2])
-          .addClass("choices-four");
-        $("#index-three")
-          .text(choicesFour[3])
-          .addClass("choices-four");
-
-        $(".choices-four").on("click", function() {
-          if ($(this).text() !== answerFour) {
-            $("#finalQuestionAccuracy").text("Incorrect!");
-          } else if ($(this).text() === answerFour) {
-            $("#finalQuestionAccuracy").text("Correct!");
-          }
-
-          $("#quizCompleted").show();
-          $("#questions-jumbotron").hide();
-          $("#score").text("You scored " + secondsLeft);
-
-          $("#initials-submit").on("click", function() {
-            $("#quizCompleted").hide();
-            $("#history").show();
-
-            $("#codeQuizHome").on("click", function() {
-              window.location.reload();
-            });
-          });
-        });
-      });
-    });
+    startTimer();
+    generateQuestion();
   });
+
+  // Start Time Function
+  function startTimer() {
+    countDown = setInterval(clockTick, 1000);
+  }
+
+  function clockTick() {
+    --secondsLeft;
+    if (secondsLeft >= 0) {
+      $("#time-remaining").text(
+        "Time remaining: " + secondsLeft + " remaining"
+      );
+    } else if (secondsLeft < 0) {
+      secondsLeft = 0;
+      endGame();
+    }
+  }
+
+  function stopTimer() {
+    countDown = clearInterval(clockTick);
+  }
+
+  function endGame() {
+    stopTimer();
+    generateScore();
+    $("#quizCompleted").show();
+    $("#questionDisplay").hide();
+    $("#choiceButtons").hide();
+    $("#questions-jumbotron").hide();
+  }
+
+  function generateQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[questionsIndex];
+
+    var titleElement = document.getElementById("questionDisplay");
+    titleElement.textContent = questions[questionsIndex].title;
+
+    var choicesElement = document.getElementById("choiceButtons");
+    choicesElement.innerHTML = "";
+
+    // Loop through all the choices and create a button for each
+    for (var i = 0; i < currentQuestion.choices.length; ++i) {
+      var choiceNode = document.createElement("button");
+
+      choiceNode.setAttribute("class", "choices");
+      choiceNode.setAttribute("value", currentQuestion.choices[i]);
+
+      choiceNode.textContent = currentQuestion.choices[i];
+
+      // attach click event listener to each choice
+      choiceNode.onclick = clickButton;
+
+      // display on the page
+      choicesElement.appendChild(choiceNode);
+    }
+  }
+
+  function clickButton() {
+    console.log(this.value);
+    if (this.value !== questions[questionsIndex].answer) {
+      // penalize time
+      secondsLeft -= 10;
+
+      // display new time on page
+      timerEl.textContent = secondsLeft;
+    }
+
+    questionsIndex++;
+
+    if (questionsIndex === questions.length) {
+      endGame();
+    } else {
+      generateQuestion();
+    }
+  }
+  function generateScore() {
+    $("#score").text("Score: " + secondsLeft);
+  }
+  document
+    .getElementById("initials-submit")
+    .addEventListener("click", showHistory);
+
+  function showHistory(event) {
+    $("#quizCompleted").append($("#scoreTitle"));
+    $("#quizCompleted").append($("#highscoreDisplay"));
+    $("#quizCompleted").append($("#codeQuizHome"));
+    $("#quizCompleted").append($("#clear"));
+  }
 });
